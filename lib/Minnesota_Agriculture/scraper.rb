667950b41743cccdcf8b6399812d9e7496be941b
Matthew Ream <matthewtimothyream@gmail.com>
2019-09-13 22:29:06 -0500

    figuring out iterations and created  agriculturedata

--- lib/Minnesota_Agriculture/scraper.rb
+++ lib/Minnesota_Agriculture/scraper.rb
@@ -4,14 +4,16 @@ class Scraper
         doc = Nokogiri::HTML(open("https://www.nass.usda.gov/Quick_Stats/Ag_Overview/stateOverview.php?state=MINNESOTA"))
         doc.css(".heading").each do |category_css|
         commodity = category_css.css(".commodity").text.strip
-        binding.pry
-        #commodity.css(".commodity").each do |production_data|
-        #planted_all_purpose_acres = production_data.css(".dataitem").text.strip
+            doc.css(".dataitem").each do |production_data|
+                    #double check on iterating within an iteration?
+                planted_all_purpose_acres = production_data.css(".dataitem").text.strip
+                    #double check on how to target a specific .dataitem with its respective data?
         #harvested_acres = 
         #iyeld =
         #production =
         #price_per_unit =
         #value_of_production_in_dollars = 
+            end
         end
     end
 end
