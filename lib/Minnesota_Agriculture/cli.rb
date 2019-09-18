7d012e4bf325331be5a26d5e3c8b42a7ed0c5dc5
Matthew Ream <matthewtimothyream@gmail.com>
2019-09-18 13:30:07 -0500

    Figuring out exit method

--- lib/Minnesota_Agriculture/cli.rb
+++ lib/Minnesota_Agriculture/cli.rb
@@ -7,19 +7,25 @@ class CLI
         puts "Loading..."
         Scraper.get_initial_data
         menu
+        exit
     end
 
     def menu
         Agriculture_Data.all.each_with_index do |row, index|
             puts "#{index + 1}. #{row.crop}"
         end
+        puts ""
         puts "Please select a crop by number to view a summary of its production data in Minnesota for 2018."
+        puts ""
         input = gets.chomp
         if input.to_i > Agriculture_Data.all.size || input.to_i <= 0
+            puts ""
             puts "That is an invalid number"
+            puts ""
             menu
         end
         agriculture_data = Agriculture_Data.all[input.to_i - 1]
+        puts ""
         puts "Here is the summary for #{agriculture_data.crop}:"
         if agriculture_data.planted_all_purpose_acres == ""
             puts "Planted All Purpose Acres = N/A"
@@ -51,11 +57,20 @@ class CLI
         else
             puts "Value of Production in Dollars = #{agriculture_data.value_of_production_in_dollars}"
         end
+        puts ""
         menu
+        exit
     end
 
-    #def exit
-        #puts "Would you like to select another crop or would you like to exit the program?"
-        #answer = gets.chomp
-    #end
+    def exit
+        puts "Would you like to 'select another crop' or would you like to 'exit the program'?"
+        answer = gets.chomp
+        if answer == "select another crop"
+            menu
+        elsif answer == "exit the program"
+        menu.exit!
+        else
+            puts "That is an invalid answer"
+        end
+    end
 end
